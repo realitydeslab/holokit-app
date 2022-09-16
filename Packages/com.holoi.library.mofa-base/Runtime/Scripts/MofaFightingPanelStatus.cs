ed05ca6153a6925770342eef6807293fc609f06a
sizheng <18861813833@163.com>
2022-09-16 16:35:58 +0800

    update loading state ui and logic

--- Packages/com.holoi.library.mofa-base/Runtime/Scripts/MofaFightingPanelStatus.cs
+++ Packages/com.holoi.library.mofa-base/Runtime/Scripts/MofaFightingPanelStatus.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Holoi.Library.HoloKitApp;
+using UnityEngine.VFX;
 
 namespace Holoi.Mofa.Base
 {
     public class MofaFightingPanelStatus : MonoBehaviour
     {
         private MofaBaseRealityManager _mofaRealityManager;
+        [SerializeField] Animator _animator;
+        [Header("UI Elements")]
+        [SerializeField] VisualEffect _attackBar;
+        [SerializeField] VisualEffect _ultimateBar;
+
 
         private void Awake()
         {
@@ -29,19 +35,19 @@ namespace Holoi.Mofa.Base
             // SIZHENGTODO:
             if (controllerState == ControllerState.Ground)
             {
-                // 二技能开始充能
+                _animator.SetTrigger("To Function");
             }
             else
             {
-                // 二技能停止充能
+                _animator.SetTrigger("To Attack");
             }
         }
 
         private void Update()
         {
+            _attackBar.SetFloat("Loading", _mofaRealityManager.LocalPlayerSpellManager.BasicSpellChargePercentage);
+            _ultimateBar.SetFloat("Loading", _mofaRealityManager.LocalPlayerSpellManager.SecondarySpellChargePercentage);
             // SIZHENGTODO: 更新充能
-            // _mofaRealityManager.LocalPlayerSpellManager.BasicSpellChargePercentage 此为基础攻击的充能百分比
-            // _mofaRealityManager.LocalPlayerSpellManager.SecondarySpellChargePercentage 此为二技能的充能百分比
             // _mofaRealityManager.LocalPlayerSpellManager.SecondarySpellUseCount 此为二技能已经使用过的次数
             // _mofaRealityManager.LocalPlayerSpellManager.SecondarySpell.MaxUseCount 此为二技能的最大使用次数
         }
