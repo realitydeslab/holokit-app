7ce08edda39644c79267d9cbbb376cdbcae1e164
sizheng <18861813833@163.com>
2022-09-21 17:20:26 +0800

    update permission panel script

--- Assets/Scripts/UI/PermissionsPanel.cs
+++ Assets/Scripts/UI/PermissionsPanel.cs
@@ -1,12 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class PermissionsPanel : MonoBehaviour
+namespace Holoi.Library.HoloKitApp.UI
 {
-    public void LoadRealitiesPage()
+    public class PermissionsPanel : MonoBehaviour
     {
-        SceneManager.LoadSceneAsync("Realities", LoadSceneMode.Single);
+        [SerializeField] Button _camButton;
+        [SerializeField] Button _microButton;
+        [SerializeField] Button _photoButton;
+        [SerializeField] Button _locationButton;
+
+        private void Start()
+        {
+            // yc todo:
+            _camButton.onClick.AddListener(() => {
+                // todo:
+            });
+        }
+
+        void UpdateAllPermisionButtons()
+        {
+            if (true)
+            {
+                _camButton.GetComponent<FlexibleUIPermissionButton>().state = FlexibleUIPermissionButton.GoOnState.Checked;
+            }
+            else
+            {
+                _camButton.GetComponent<FlexibleUIPermissionButton>().state = FlexibleUIPermissionButton.GoOnState.Uncheck;
+            }
+
+            if (true)
+            {
+                _microButton.GetComponent<FlexibleUIPermissionButton>().state = FlexibleUIPermissionButton.GoOnState.Checked;
+            }
+            else
+            {
+                _microButton.GetComponent<FlexibleUIPermissionButton>().state = FlexibleUIPermissionButton.GoOnState.Uncheck;
+            }
+
+            if (true)
+            {
+                _photoButton.GetComponent<FlexibleUIPermissionButton>().state = FlexibleUIPermissionButton.GoOnState.Checked;
+            }
+            else
+            {
+                _photoButton.GetComponent<FlexibleUIPermissionButton>().state = FlexibleUIPermissionButton.GoOnState.Uncheck;
+            }
+
+            if (true)
+            {
+                _locationButton.GetComponent<FlexibleUIPermissionButton>().state = FlexibleUIPermissionButton.GoOnState.Checked;
+            }
+            else
+            {
+                _locationButton.GetComponent<FlexibleUIPermissionButton>().state = FlexibleUIPermissionButton.GoOnState.Uncheck;
+            }
+        }
     }
 }
