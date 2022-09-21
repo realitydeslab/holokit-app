698a2fc6dbb4de10a7daecae0c46cca451152a9e
yuchen <yuchen@holoi.com>
2022-09-21 17:04:12 +0800

    Localization Algorithm

--- Packages/com.holoi.library.holokit-app/Runtime/Scripts/Core/RealityManager.cs
+++ Packages/com.holoi.library.holokit-app/Runtime/Scripts/Core/RealityManager.cs
@@ -14,6 +14,18 @@ namespace Holoi.Library.HoloKitApp
 
         public List<GameObject> NetworkPrefabs;
 
+        private Vector3 _lastImagePosition;
+
+        private Quaternion _lastImageRotation;
+
+        private int _imageStablizationFrameCount = 0;
+
+        private const float ImageStablizationPositionThreshold = 0.03f;
+
+        private const float ImageStablizationRotationThreshold = 7;
+
+        private const int ImageStabilizationFrameNum = 80;
+
         public static event Action<RealityManager> OnRealityManagerSpawned;
 
         public static event Action OnFinishedScanningQRCode;
@@ -42,13 +54,50 @@ namespace Holoi.Library.HoloKitApp
         private void StartScanningQRCode()
         {
             var arTrackedImageManager = HoloKitCamera.Instance.GetComponentInParent<ARTrackedImageManager>(true);
+            arTrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
             arTrackedImageManager.enabled = true;
+        }
+
+        // TODO: Design a better localization algorithm
+        private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs args)
+        {
+            foreach (var image in args.added)
+            {
+                _imageStablizationFrameCount = 0;
+            }
+
+            foreach (var image in args.updated)
+            {
+                if (_imageStablizationFrameCount == 0)
+                {
+                    _lastImagePosition = image.transform.position;
+                    _lastImageRotation = image.transform.rotation;
+                }
+
+                if (Vector3.Distance(image.transform.position, _lastImagePosition) < ImageStablizationPositionThreshold &&
+                    Quaternion.Angle(image.transform.rotation, _lastImageRotation) < ImageStablizationRotationThreshold)
+                {
+                    _imageStablizationFrameCount++;
+                    Debug.Log($"[RealityManager] image stablization frame count {_imageStablizationFrameCount}");
+                    if (_imageStablizationFrameCount == ImageStabilizationFrameNum)
+                    {
+
+                    }
+                }
+                else
+                {
+                    _imageStablizationFrameCount = 0;
+                }
+            }
         }
 
         private void StopScanningQRCode()
         {
             var arTrackedImageManager = HoloKitCamera.Instance.GetComponentInParent<ARTrackedImageManager>();
+            arTrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
             arTrackedImageManager.enabled = false;
         }
+
+
     }
 }
