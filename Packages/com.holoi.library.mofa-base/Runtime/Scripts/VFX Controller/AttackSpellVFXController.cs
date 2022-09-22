2a97066ee24963a293082545c65e5c15e485de2d
sizheng <18861813833@163.com>
2022-09-22 16:48:43 +0800

    update
    add spell visual controller

---
+++ Packages/com.holoi.library.mofa-base/Runtime/Scripts/VFX Controller/AttackSpellVFXController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Holoi.Mofa.Base
+{
+    public class AttackSpellVFXController : MonoBehaviour
+    {
+        [SerializeField] AttackSpell _attackController;
+        [SerializeField] SpellLifetimeController _lifeTimeController;
+        [SerializeField] Animator _animator;
+
+        private void OnEnable()
+        {
+            if (_attackController == null) Debug.LogError("not found attackSpell");
+            _attackController.OnHit += OnHit;
+            _lifeTimeController.OnSpawned += OnSpawn;
+            _lifeTimeController.OnDead += OnDie;
+        }
+
+        private void OnDisable()
+        {
+            _attackController.OnHit -= OnHit;
+            _lifeTimeController.OnSpawned -= OnSpawn;
+            _lifeTimeController.OnDead -= OnDie;
+
+        }
+
+        void OnSpawn()
+        {
+
+        }
+        void OnHit()
+        {
+            _animator.SetTrigger("Hit");
+        }
+
+        void OnDie()
+        {
+            _animator.SetTrigger("Die");
+        }
+    }
+}
