697f4e6b5d323b75bbef9f7b762355fbf5b4c95c
yuchen <yuchen@holoi.com>
2022-09-23 17:03:46 +0800

    Updated

--- Packages/com.holoi.library.mofa-base/Runtime/Scripts/MofaPopupManager.cs
+++ Packages/com.holoi.library.mofa-base/Runtime/Scripts/MofaPopupManager.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using HoloKit;
+using Holoi.Library.HoloKitApp;
 
 namespace Holoi.Mofa.Base
 {
     public class MofaPopupManager : MonoBehaviour
     {
-        [SerializeField] private Vector3 _centerEyeOffset;
-
         [SerializeField] private GameObject _countdownPrefab;
 
         [SerializeField] private GameObject _roundOverPrefab;
@@ -17,13 +16,21 @@ namespace Holoi.Mofa.Base
 
         [SerializeField] private GameObject _defeatPrefab;
 
+        [SerializeField] private GameObject _drawPrefab;
+
         [SerializeField] private GameObject _deathPrefab;
 
+        [SerializeField] private GameObject _summaryBoardPrefab;
+
         private GameObject _currentPopup;
+
+        private MofaFightingPanel _mofaFightingPanel;
 
         private void Awake()
         {
             MofaBaseRealityManager.OnPhaseChanged += OnPhaseChanged;
+
+            _mofaFightingPanel = HoloKitCamera.Instance.CenterEyePose.GetComponentInChildren<MofaFightingPanel>();
         }
 
         private void OnDestroy()
@@ -33,9 +40,13 @@ namespace Holoi.Mofa.Base
 
         private IEnumerator SpawnPopup(GameObject popupPrefab, float destroyDelay)
         {
+            if (_currentPopup != null)
+            {
+                Destroy(_currentPopup);
+            }
             _currentPopup = Instantiate(popupPrefab);
-            _currentPopup.transform.SetParent(HoloKitCamera.Instance.CenterEyePose);
-            _currentPopup.transform.localPosition = _centerEyeOffset;
+            _currentPopup.transform.SetParent(_mofaFightingPanel.transform);
+            _currentPopup.transform.localPosition = Vector3.zero;
             _currentPopup.transform.localRotation = Quaternion.identity;
 
             yield return new WaitForSeconds(destroyDelay);
@@ -57,13 +68,57 @@ namespace Holoi.Mofa.Base
                 case MofaPhase.Fighting:
                     break;
                 case MofaPhase.RoundOver:
+                    StartCoroutine(SpawnPopup(_roundOverPrefab, 4f));
                     break;
                 case MofaPhase.RoundResult:
-
+                    OnRoundResult();
                     break;
                 case MofaPhase.RoundData:
+                    OnRoundData();
                     break;
             }
+        }
+
+        private void OnRoundResult()
+        {
+            var mofaRealityManager = HoloKitApp.Instance.RealityManager as MofaBaseRealityManager;
+            var localPlayer = mofaRealityManager.GetLocalPlayer();
+            if (localPlayer == null)
+            {
+                return;
+            }
+            var roundResult = mofaRealityManager.GetRoundResult();
+            switch (roundResult)
+            {
+                case MofaRoundResult.BlueTeamWins:
+                    if (localPlayer.Team.Value == MofaTeam.Blue)
+                    {
+                        StartCoroutine(SpawnPopup(_victoryPrefab, 3f));
+                    }
+                    else
+                    {
+                        StartCoroutine(SpawnPopup(_defeatPrefab, 3f));
+                    }
+                    break;
+                case MofaRoundResult.RedTeamWins:
+                    if (localPlayer.Team.Value == MofaTeam.Blue)
+                    {
+                        StartCoroutine(SpawnPopup(_defeatPrefab, 3f));
+                    }
+                    else
+                    {
+                        StartCoroutine(SpawnPopup(_victoryPrefab, 3f));
+                    }
+                    break;
+                case MofaRoundResult.Draw:
+                    StartCoroutine(SpawnPopup(_drawPrefab, 3f));
+                    break;
+            }
+        }
+
+        private void OnRoundData()
+        {
+            StartCoroutine(SpawnPopup(_summaryBoardPrefab, 30f));
         }
     }
 }
