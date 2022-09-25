9d4056e4554f5ef5c6bbf4845cbd81a82b07a628
yuchen <yuchen@holoi.com>
2022-09-25 18:59:24 +0800

    Deleted Fusion code and other useless stuff

--- Assets/Photon/FusionCodeGen/Fusion.CodeGen.User.cs
+++
@@ -1,9 +0,0 @@
-#if FUSION_WEAVER && FUSION_WEAVER_ILPOSTPROCESSOR
-namespace Fusion.CodeGen {
-  static partial class ILWeaverSettings {
-
-    static partial void OverrideNetworkProjectConfigPath(ref string path) {
-    }
-  }
-}
-#endif
