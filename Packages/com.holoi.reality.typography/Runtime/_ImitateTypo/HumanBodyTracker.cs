f2f964193c485247b7fbe8ca19d2b33466bccb22
sizheng <18861813833@163.com>
2022-10-09 17:21:47 +0800

    back up typo human imitate reality

---
+++ Packages/com.holoi.reality.typography/Runtime/_ImitateTypo/HumanBodyTracker.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using HoloKit;
+using UnityEngine.VFX;
+
+namespace UnityEngine.XR.ARFoundation.Samples
+{
+    public class HumanBodyTracker : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("The Skeleton prefab to be controlled.")]
+        GameObject m_SkeletonPrefab;
+
+        [SerializeField]
+        [Tooltip("The ARHumanBodyManager which will produce body tracking events.")]
+        ARHumanBodyManager m_HumanBodyManager;
+
+        float _process = 0;
+        /// <summary>
+        /// Get/Set the <c>ARHumanBodyManager</c>.
+        /// </summary>
+        public ARHumanBodyManager humanBodyManager
+        {
+            get { return m_HumanBodyManager; }
+            set { m_HumanBodyManager = value; }
+        }
+
+        /// <summary>
+        /// Get/Set the skeleton prefab.
+        /// </summary>
+        public GameObject skeletonPrefab
+        {
+            get { return m_SkeletonPrefab; }
+            set { m_SkeletonPrefab = value; }
+        }
+
+        Dictionary<TrackableId, BoneController> m_SkeletonTracker = new Dictionary<TrackableId, BoneController>();
+
+        void OnEnable()
+        {
+            Debug.Assert(m_HumanBodyManager != null, "Human body manager is required.");
+            m_HumanBodyManager.humanBodiesChanged += OnHumanBodiesChanged;
+        }
+
+        void OnDisable()
+        {
+            if (m_HumanBodyManager != null)
+                m_HumanBodyManager.humanBodiesChanged -= OnHumanBodiesChanged;
+        }
+
+        void OnHumanBodiesChanged(ARHumanBodiesChangedEventArgs eventArgs)
+        {
+            BoneController boneController;
+
+            foreach (var humanBody in eventArgs.added)
+            {
+                Debug.Log("e height = " + humanBody.estimatedHeightScaleFactor);
+                if (!m_SkeletonTracker.TryGetValue(humanBody.trackableId, out boneController))
+                {
+                    Debug.Log($"Adding a new skeleton [{humanBody.trackableId}].");
+                    var newSkeletonGO = Instantiate(m_SkeletonPrefab, humanBody.transform);
+                    
+                    boneController = newSkeletonGO.GetComponent<BoneController>();
+                    m_SkeletonTracker.Add(humanBody.trackableId, boneController);
+
+                    if (boneController.SkinnerVfx.GetSkinnedMeshRenderer("SkinnedMeshRenderer") == null)
+                    {
+                        boneController.SkinnerVfx.SetSkinnedMeshRenderer("SkinnedMeshRenderer", boneController.SkinnerMeshRendererRef);
+                    }
+
+                    _process += Time.deltaTime* 0.5f;
+                    boneController.SkinnerVfx.transform.localPosition = new Vector3(0.5f, 0, 0);
+                    boneController.SkinnerVfx.SetFloat("Spawn Rate", _process);
+
+                    //boneController.SkinnerVfx.transform.InverseTransformPoint(HoloKitCamera.Instance.CenterEyePose.right * 0.5f);
+                }
+
+                boneController.InitializeSkeletonJoints();
+                boneController.ApplyBodyPose(humanBody);
+            }
+
+            foreach (var humanBody in eventArgs.updated)
+            {
+                if (m_SkeletonTracker.TryGetValue(humanBody.trackableId, out boneController))
+                {
+                    boneController.ApplyBodyPose(humanBody);
+                }
+            }
+
+            foreach (var humanBody in eventArgs.removed)
+            {
+                Debug.Log($"Removing a skeleton [{humanBody.trackableId}].");
+                if (m_SkeletonTracker.TryGetValue(humanBody.trackableId, out boneController))
+                {
+                    Destroy(boneController.gameObject);
+                    m_SkeletonTracker.Remove(humanBody.trackableId);
+                }
+            }
+        }
+    }
+}
