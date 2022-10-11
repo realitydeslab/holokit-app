ce1218e1d441c65cb8cdad5549c34f4447900574
yuchen <yuchen@holoi.com>
2022-10-11 20:41:54 +0800

    Deleted legacy WatchApp folder

--- Packages/com.holoi.library.holokit-app/Runtime/Scripts/Apple Watch/HoloKitAppWatchAPI.cs
+++ Packages/com.holoi.library.holokit-app/Runtime/Scripts/Apple Watch/HoloKitAppWatchAPI.cs
@@ -1,10 +1,15 @@
 using System.Runtime.InteropServices;
 
-namespace Holoi.Library.HoloKitApp
+namespace Holoi.Library.HoloKitApp.Watch
 {
     public static class HoloKitAppWatchAPI
     {
         [DllImport("__Internal")]
         private static extern void HoloKitAppWatch_InitializeWithRealityId(int realityId);
+
+        public static void InitializeWithRealityId(int realityId)
+        {
+            HoloKitAppWatch_InitializeWithRealityId(realityId);
+        }
     }
 }
