e4da352e2d96a13826542180f503bd4cb612619e
yuchen <yuchen@holoi.com>
2022-10-13 17:15:54 +0800

    Backup

---
+++ Packages/com.holoi.library.holokit-app/Runtime/Scripts/UI/UIPanel/HoloKitAppUIPanel_SignInPage.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Holoi.Library.HoloKitApp.UI
+{
+    public class HoloKitAppUIPanel_SignInPage : HoloKitAppUIPanel
+    {
+        public override string UIPanelName => "SignInPage";
+
+        public override bool OverlayPreviousPanel => true;
+    }
+}
