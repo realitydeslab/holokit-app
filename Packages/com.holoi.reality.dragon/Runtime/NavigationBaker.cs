e4e341d8c68bc7a8d42b269513380bcfdc12a86c
sizheng <18861813833@163.com>
2022-10-13 17:27:24 +0800

    debug:
    delete duplicated phase source to debug quantum buddhas

---
+++ Packages/com.holoi.reality.dragon/Runtime/NavigationBaker.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Holoi.Reality.Dragon
+{
+    public class NavigationBaker : MonoBehaviour
+    {
+        //public NavMeshSurface[] surfaces;
+
+        // Use this for initialization
+        void Start()
+        {
+            //for (int i = 0; i < surfaces.Length; i++)
+            //{
+            //    surfaces[i].BuildNavMesh();
+            //}
+        }
+
+    }
+}
