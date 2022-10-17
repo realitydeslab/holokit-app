6302b1769da32b5068eee9b21ca810ffc0e849eb
yuchen <yuchen@holoi.com>
2022-10-17 10:37:39 +0800

    iPhone14 adapted

--- Packages/com.holoi.xr.holokit/Runtime/HoloKitTrackedPoseDriver.cs
+++ Packages/com.holoi.xr.holokit/Runtime/HoloKitTrackedPoseDriver.cs
@@ -4,6 +4,8 @@ namespace HoloKit
 {
     public class HoloKitTrackedPoseDriver : MonoBehaviour
     {
+        private readonly Matrix4x4 RotationMatrix = Matrix4x4.Rotate(Quaternion.Euler(0f, 0f, 90f));
+
         private void Awake()
         {
             HoloKitARSessionControllerAPI.OnARSessionUpdatedFrame += OnARSessionUpdatedFrame;
@@ -16,6 +18,10 @@ namespace HoloKit
 
         private void OnARSessionUpdatedFrame(double timestamp, Matrix4x4 matrix)
         {
+            if (HoloKitCamera.Instance.RenderMode == HoloKitRenderMode.Stereo)
+            {
+                matrix = RotationMatrix * matrix;
+            }
             transform.SetPositionAndRotation(matrix.GetPosition(),
                                              matrix.rotation);
         }
