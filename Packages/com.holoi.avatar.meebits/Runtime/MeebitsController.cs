2caf197207397afe406f40fa8a8487b383298b0e
sizheng <18861813833@163.com>
2022-10-27 21:26:57 +0800

    backup thumbnail assets

--- Packages/com.holoi.avatar.meebits/Runtime/MeebitsController.cs
+++ Packages/com.holoi.avatar.meebits/Runtime/MeebitsController.cs
@@ -8,13 +8,15 @@ namespace Holoi.Avatar.Meebits
     {
         public Transform HandJoint;
         public GameObject WeaponPrefab;
+        public Vector3 Offset= new Vector3(-.025f, 0.05f, 0.01f);
+        public float Scale = 0.2f;
 
         void Start()
         {
             var go = Instantiate(WeaponPrefab, HandJoint);
-            go.transform.localPosition = new Vector3(-.025f, 0.05f, 0.01f);
+            go.transform.localPosition = Offset;
             go.transform.localEulerAngles = new Vector3(-90, 0, 0);
-            go.transform.localScale = Vector3.one * 0.2f;
+            go.transform.localScale = Vector3.one * Scale;
         }
     }
 }
