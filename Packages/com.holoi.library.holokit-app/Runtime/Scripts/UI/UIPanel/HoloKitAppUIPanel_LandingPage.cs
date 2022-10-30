8df73006b1159bd46c7da97c05f509f9010f6023
yuchen <yuchen@holoi.com>
2022-10-30 17:55:37 +0800

    Added LeanTween

--- Packages/com.holoi.library.holokit-app/Runtime/Scripts/UI/UIPanel/HoloKitAppUIPanel_LandingPage.cs
+++ Packages/com.holoi.library.holokit-app/Runtime/Scripts/UI/UIPanel/HoloKitAppUIPanel_LandingPage.cs
@@ -1,3 +1,6 @@
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace Holoi.Library.HoloKitApp.UI
 {
     public class HoloKitAppUIPanel_LandingPage : HoloKitAppUIPanel
@@ -6,11 +9,42 @@ namespace Holoi.Library.HoloKitApp.UI
 
         public override bool OverlayPreviousPanel => true;
 
-        private const float Duration = 2f;
+        [SerializeField] private Image _logoImage;
+
+        [SerializeField] private Image _sloganImage;
+
+        [SerializeField] private Image _headsetImage;
+
+        private const float PageDuration = 2f;
+
+        private const float LogoAppearTime = 0.6f;
+
+        private const float SloganAppearDelay = 0.6f;
+
+        private const float SloganAppearTime = 0.6f;
+
+        private const float HeadsetEndPosY = -960f;
+
+        private const float HeadsetMovementDuration = 1.2f;
 
         private void Start()
         {
-            StartCoroutine(HoloKitAppUtils.WaitAndDo(Duration, () =>
+            // Logo
+            _logoImage.color = new Color(1f, 1f, 1f, 0f);
+            LeanTween.alpha(_logoImage.rectTransform, 1f, LogoAppearTime)
+                .setEase(LeanTweenType.linear);
+
+            // Slogan
+            _logoImage.color = new Color(1f, 1f, 1f, 0f);
+            LeanTween.alpha(_sloganImage.rectTransform, 1f, SloganAppearTime)
+                .setEase(LeanTweenType.linear)
+                .setDelay(SloganAppearDelay);
+
+            // Headset
+            LeanTween.moveY(_headsetImage.rectTransform, HeadsetEndPosY, HeadsetMovementDuration)
+                .setEase(LeanTweenType.easeSpring);
+
+            StartCoroutine(HoloKitAppUtils.WaitAndDo(PageDuration, () =>
             {
                 LoadPermissionPage();
             }));
