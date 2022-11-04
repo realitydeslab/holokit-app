b7b42b2bf5a71816922202e9c7e8a62eea659492
yuchen <yuchen@holoi.com>
2022-11-04 17:17:31 +0800

    TypedRealityTheScanner checked

---
+++ Packages/com.holoi.reality.typography/Runtime/_SpaceTypo/UI/TypedRealityTheScannerUIPanel.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using Holoi.Library.HoloKitApp;
+using Holoi.Library.HoloKitApp.UI;
+using TMPro;
+
+namespace Holoi.Reality.Typography
+{
+    public class TypedRealityTheScannerUIPanel : HoloKitAppUIPanel
+    {
+        public override string UIPanelName => "TypedRealityTheScanner";
+
+        public override bool OverlayPreviousPanel => false;
+
+        [SerializeField] private GameObject _text;
+
+        [SerializeField] private Color _textColor;
+
+        private void Start()
+        {
+            Color newColor = new(_textColor.r, _textColor.g, _textColor.b, 0f);
+            LeanTween.value(_text, (Color color) => { _text.GetComponent<TMP_Text>().color = color; }, _textColor, newColor, 2f)
+                .setEase(LeanTweenType.easeInOutSine)
+                .setDelay(4f)
+                .setOnComplete(() =>
+                {
+                    _text.SetActive(false);
+                });
+        }
+    }
+}
