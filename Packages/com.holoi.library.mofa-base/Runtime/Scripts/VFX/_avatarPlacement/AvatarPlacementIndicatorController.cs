592635494985331eeafd42689758701284f0b462
sizheng <18861813833@163.com>
2022-11-06 16:30:13 +0800

    backup

--- Packages/com.holoi.library.mofa-base/Runtime/Scripts/VFX/_avatarPlacement/AvatarPlacementIndicatorController.cs
+++ Packages/com.holoi.library.mofa-base/Runtime/Scripts/VFX/_avatarPlacement/AvatarPlacementIndicatorController.cs
@@ -6,22 +6,19 @@ namespace Holoi.Library.MOFABase
     public class AvatarPlacementIndicatorController : MonoBehaviour
     {
         [SerializeField] Animator _animator;
-
         [SerializeField] VisualEffect _placementVFX;
-
         [SerializeField] VisualEffect _birthVFX;
 
-        //void Start()
-        //{
-        //    _placementVFX.enabled = true;
-        //    _birthVFX.enabled = false;
-
-        //}
+        void Start()
+        {
+            _placementVFX.enabled = true;
+            _birthVFX.enabled = false;
+        }
 
         public void OnBirth()
         {
-            //_animator.SetTrigger("Birth");
-            //_birthVFX.enabled = true;
+            _animator.SetTrigger("Birth");
+            _birthVFX.enabled = true;
         }
     }
 }
