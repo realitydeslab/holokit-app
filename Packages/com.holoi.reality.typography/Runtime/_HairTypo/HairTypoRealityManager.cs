32c0a06d63d58dca02c5674bc45b6433c2385fbf
yuchen <yuchen@holoi.com>
2022-11-07 17:51:43 +0800

    Refactored The Hair

--- Packages/com.holoi.reality.typography/Runtime/_HairTypo/HairTypoRealityManager.cs
+++ Packages/com.holoi.reality.typography/Runtime/_HairTypo/HairTypoRealityManager.cs
@@ -1,55 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Unity.Netcode;
-using HoloKit;
 using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.ARSubsystems;
 using Holoi.Library.HoloKitApp;
-using Holoi.Library.ARUX;
 
 namespace Holoi.Reality.Typography
 {
     public class HairTypoRealityManager : RealityManager
     {
-        [Header("AR Base Objects")]
-        [SerializeField] Transform _serverCenterEye;
-
-        Transform _centerEye;
-
-        [HideInInspector] public Vector3 HitPosition = Vector3.down;
-
-        public override void OnNetworkSpawn()
-        {
-            base.OnNetworkSpawn();
-
-        }
-
+        [Header("AR")]
+        [SerializeField] private AROcclusionManager _arOcclusionManager;
 
         private void Start()
         {
-            if (_centerEye == null) _centerEye = HoloKitCamera.Instance.CenterEyePose;
-
-            InitServerCenterEye();
-        }
-
-        void Update()
-        {
-
-
-        }
-
-        void InitServerCenterEye()
-        {
-
-            if (HoloKitApp.Instance.IsHost)
+            if (HoloKitApp.Instance.IsSpectator)
             {
-                _serverCenterEye.GetComponent<FollowMovementManager>().enabled = true;
-            }
-            else
-            {
-                _serverCenterEye.GetComponent<FollowMovementManager>().enabled = false;
-
+                _arOcclusionManager.enabled = true;
             }
         }
     }
