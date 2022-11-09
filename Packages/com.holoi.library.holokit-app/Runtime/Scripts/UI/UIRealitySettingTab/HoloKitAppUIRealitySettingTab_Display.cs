f7c350d32d9107b5f3d7a248f079494bdf7f5852
yuchen <yuchen@holoi.com>
2022-11-09 17:14:06 +0800

    Human occlusion toggle

--- Packages/com.holoi.library.holokit-app/Runtime/Scripts/UI/UIRealitySettingTab/HoloKitAppUIRealitySettingTab_Display.cs
+++ Packages/com.holoi.library.holokit-app/Runtime/Scripts/UI/UIRealitySettingTab/HoloKitAppUIRealitySettingTab_Display.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 using TMPro;
+using HoloKit;
 
 namespace Holoi.Library.HoloKitApp.UI
 {
@@ -9,15 +11,39 @@ namespace Holoi.Library.HoloKitApp.UI
 
         [SerializeField] private TMP_Text _occlusionStatus;
 
+        [SerializeField] private HoloKitAppUIComponent_AnimatedToggle _occlusionToggle;
+
+        private AROcclusionManager _arOcclusionManager;
+
         private void Start()
         {
-            
+            _arOcclusionManager = HoloKitCamera.Instance.GetComponent<AROcclusionManager>();
+            if (_arOcclusionManager != null
+                && _arOcclusionManager.requestedHumanDepthMode != UnityEngine.XR.ARSubsystems.HumanSegmentationDepthMode.Disabled
+                && _arOcclusionManager.requestedHumanStencilMode != UnityEngine.XR.ARSubsystems.HumanSegmentationStencilMode.Disabled
+                && _arOcclusionManager.requestedOcclusionPreferenceMode != UnityEngine.XR.ARSubsystems.OcclusionPreferenceMode.PreferHumanOcclusion)
+            {
+                _occlusionToggle.Toggled = true;
+                _occlusionStatus.text = "On";
+            }
         }
 
         public void OnOcclusionToggleValueChanged(bool toggled)
         {
-            _occlusionStatus.text = toggled ? "On" : "Off";
-            HoloKitAppUIEventManager.OnHumanOcclusionToggled?.Invoke(toggled);
+            if (toggled)
+            {
+                _occlusionStatus.text = "On";
+                _arOcclusionManager.requestedHumanDepthMode = UnityEngine.XR.ARSubsystems.HumanSegmentationDepthMode.Fastest;
+                _arOcclusionManager.requestedHumanStencilMode = UnityEngine.XR.ARSubsystems.HumanSegmentationStencilMode.Fastest;
+                _arOcclusionManager.requestedOcclusionPreferenceMode = UnityEngine.XR.ARSubsystems.OcclusionPreferenceMode.PreferHumanOcclusion;
+            }
+            else
+            {
+                _occlusionStatus.text = "Off";
+                _arOcclusionManager.requestedHumanDepthMode = UnityEngine.XR.ARSubsystems.HumanSegmentationDepthMode.Disabled;
+                _arOcclusionManager.requestedHumanStencilMode = UnityEngine.XR.ARSubsystems.HumanSegmentationStencilMode.Disabled;
+                _arOcclusionManager.requestedOcclusionPreferenceMode = UnityEngine.XR.ARSubsystems.OcclusionPreferenceMode.NoOcclusion;
+            }
         }
     }
 }
