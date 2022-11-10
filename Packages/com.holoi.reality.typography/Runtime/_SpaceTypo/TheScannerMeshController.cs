31f7c3c90bbdc1dcf782757cd3967aeb40d640b1
yuchen <yuchen@holoi.com>
2022-11-10 17:51:22 +0800

    TheScanner center translation and rotation

--- Packages/com.holoi.reality.typography/Runtime/_SpaceTypo/TheScannerMeshController.cs
+++ Packages/com.holoi.reality.typography/Runtime/_SpaceTypo/TheScannerMeshController.cs
@@ -12,5 +12,14 @@ namespace Holoi.Reality.Typography
         {
             _material = GetComponent<MeshRenderer>().material;
         }
+
+        private void Update()
+        {
+            if (TheScannerCenter.Instance != null)
+            {
+                _material.SetVector("_Center_Position", TheScannerCenter.Instance.transform.position);
+                _material.SetFloat("_Angle_Offset", TheScannerCenter.Instance.transform.rotation.eulerAngles.y);
+            }
+        }
     }
 }
