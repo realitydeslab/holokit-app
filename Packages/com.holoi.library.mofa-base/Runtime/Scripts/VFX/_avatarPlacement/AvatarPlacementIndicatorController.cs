6e649d433b9b691f64f2e0939c597f57d3362c84
shibi <18861813833@163.com>
2022-11-13 21:32:58 +0800

    update mofa indicator

--- Packages/com.holoi.library.mofa-base/Runtime/Scripts/VFX/_avatarPlacement/AvatarPlacementIndicatorController.cs
+++ Packages/com.holoi.library.mofa-base/Runtime/Scripts/VFX/_avatarPlacement/AvatarPlacementIndicatorController.cs
@@ -5,32 +5,46 @@ namespace Holoi.Library.MOFABase
 {
     public class AvatarPlacementIndicatorController : MonoBehaviour
     {
+        [HideInInspector] public Vector3 HitPosition;
+
         [SerializeField] Animator _animator;
+
+        [SerializeField] VisualEffect _hookVFX;
 
         [SerializeField] VisualEffect _placementVFX;
 
         [SerializeField] VisualEffect _birthVFX;
 
+        Transform _centerEye;
+
         private void Start()
         {
+            _centerEye = HoloKit.HoloKitCamera.Instance.CenterEyePose;
+
+            _hookVFX.enabled = true;
+
             _placementVFX.enabled = true;
+
             _birthVFX.enabled = false;
         }
 
-        //public void OnAppear()
-        //{
-        //    _placementVFX.enabled = true;
-        //    _birthVFX.enabled = false;
-        //}
+        private void Update()
+        {
+            var pos = new Vector3(_centerEye.position.x, HitPosition.y, _centerEye.position.z);
 
-        //public void OnDisappear()
-        //{
-        //    _placementVFX.enabled = false;
-        //}
+            transform.position = pos;
+
+            _placementVFX.transform.position = HitPosition;
+
+            _birthVFX.transform.position = HitPosition;
+
+            _hookVFX.SetVector3("Hit Position", HitPosition);
+        }
 
         public void OnBirth()
         {
             _birthVFX.enabled = true;
+
             _animator.SetTrigger("Birth");
         }
     }
