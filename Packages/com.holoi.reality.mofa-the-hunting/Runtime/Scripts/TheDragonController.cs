1cc35a67b0c6693ecddbd87b5b7646908bd2dc3d
shibi <18861813833@163.com>
2022-11-18 17:54:57 +0800

    bakcup

--- Packages/com.holoi.reality.mofa-the-hunting/Runtime/Scripts/TheDragonController.cs
+++ Packages/com.holoi.reality.mofa-the-hunting/Runtime/Scripts/TheDragonController.cs
@@ -254,7 +254,6 @@ namespace Holoi.Reality.MOFATheHunting
         {
             _dragonExplodeVfx.SetVector3("HitPosition", hitPosition);
             _dragonExplodeVfx.SendEvent("OnHit");
-            Debug.Log("hit: " + hitPosition);
         }
 
         #region Network Callbacks
