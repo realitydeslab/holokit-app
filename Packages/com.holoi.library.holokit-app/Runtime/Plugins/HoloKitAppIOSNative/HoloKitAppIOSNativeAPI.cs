e4c49c4211f03094c53a3e25a5401f5e76a6c185
yuchen <yuchen@holoi.com>
2022-11-18 11:34:16 +0800

    Updated

---
+++ Packages/com.holoi.library.holokit-app/Runtime/Plugins/HoloKitAppIOSNative/HoloKitAppIOSNativeAPI.cs
@@ -0,0 +1,26 @@
+using System.Runtime.InteropServices;
+
+namespace Holoi.Library.HoloKitApp.IOSNative
+{
+    public static class HoloKitAppIOSNativeAPI
+    {
+        [DllImport("__Internal")]
+        private static extern void HoloKitAppIOSNative_OpenURL(string url);
+
+        [DllImport("__Internal")]
+        private static extern void HoloKitAppIOSNative_ShowFeedbackAlert(string title, string message, string actionTile, string url);
+
+        public static void OpenURL(string url)
+        {
+            HoloKitAppIOSNative_OpenURL(url);
+        }
+
+        public static void ShowFeedbackAlert()
+        {
+            HoloKitAppIOSNative_ShowFeedbackAlert("Feedback",
+                "We are currently under Beta testing, please go to our discord to give your feedback.",
+                "Go to Discord",
+                "https://discord.gg/nsPPBfAJ2f");
+        }
+    }
+}
--- Packages/com.holoi.library.holokit-app/Runtime/Scripts/UI/UIPanel/HoloKitAppUIPanel_MenuPage.cs
+++ Packages/com.holoi.library.holokit-app/Runtime/Scripts/UI/UIPanel/HoloKitAppUIPanel_MenuPage.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using Holoi.Library.HoloKitApp.iOSOpenURL;
+using Holoi.Library.HoloKitApp.IOSNative;
 
 namespace Holoi.Library.HoloKitApp.UI
 {
@@ -35,14 +35,14 @@ namespace Holoi.Library.HoloKitApp.UI
         public void OnIntroButtonPressed()
         {
             //Application.OpenURL("https://holokit.io");
-            iOSOpenURLAPI.OpenURL("https://holokit.io");
+            HoloKitAppIOSNativeAPI.OpenURL("https://holokit.io");
         }
 
         public void OnBuyHoloKitButtonPressed()
         {
             //HoloKitApp.Instance.UIPanelManager.PushUIPanel("BuyHoloKitPage");
             //Application.OpenURL("https://holokit.io/x/get-it-now/");
-            iOSOpenURLAPI.OpenURL("https://holokit.io/x/get-it-now/");
+            HoloKitAppIOSNativeAPI.OpenURL("https://holokit.io/x/get-it-now/");
         }
 
         public void OnGettingStartedButtonPressed()
@@ -60,34 +60,39 @@ namespace Holoi.Library.HoloKitApp.UI
             HoloKitApp.Instance.UIPanelManager.PushUIPanel("SettingsPage");
         }
 
+        public void OnFeedbackButtonPressed()
+        {
+            HoloKitAppIOSNativeAPI.ShowFeedbackAlert();
+        }
+
         public void OnTwitterButtonPressed()
         {
             //Application.OpenURL("https://twitter.com/holokit_io");
-            iOSOpenURLAPI.OpenURL("https://twitter.com/holokit_io");
+            HoloKitAppIOSNativeAPI.OpenURL("https://twitter.com/holokit_io");
         }
 
         public void OnDiscordButtonPressed()
         {
             //Application.OpenURL("https://discord.gg/nsPPBfAJ2f");
-            iOSOpenURLAPI.OpenURL("https://discord.gg/nsPPBfAJ2f");
+            HoloKitAppIOSNativeAPI.OpenURL("https://discord.gg/nsPPBfAJ2f");
         }
 
         public void OnInstagramButtonPressed()
         {
             //Application.OpenURL("https://www.instagram.com/holokit.io/");
-            iOSOpenURLAPI.OpenURL("https://www.instagram.com/holokit.io/");
+            HoloKitAppIOSNativeAPI.OpenURL("https://www.instagram.com/holokit.io/");
         }
 
         public void OnTiktokButtonPressed()
         {
             //Application.OpenURL("https://www.tiktok.com/@holokit.io");
-            iOSOpenURLAPI.OpenURL("https://www.tiktok.com/@holokit.io");
+            HoloKitAppIOSNativeAPI.OpenURL("https://www.tiktok.com/@holokit.io");
         }
 
         public void OnYoutubeButtonPressed()
         {
             //Application.OpenURL("https://www.youtube.com/channel/UCNLdtATBRfPlKbdsTb1Y-5Q");
-            iOSOpenURLAPI.OpenURL("https://www.youtube.com/channel/UCNLdtATBRfPlKbdsTb1Y-5Q");
+            HoloKitAppIOSNativeAPI.OpenURL("https://www.youtube.com/channel/UCNLdtATBRfPlKbdsTb1Y-5Q");
         }
 
         public void OnMirrorButtonPressed()
