d2557da1dc2719d8f2333ba0bee27f1092a19c2b
yuchen <yuchen@holoi.com>
2022-11-29 17:08:12 +0800

    Updated

---
+++ Packages/com.holoi.library.mofa-base/Runtime/Scripts/UI/MofaUIPanel.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using Holoi.Library.HoloKitApp.UI;
+
+namespace Holoi.Library.MOFABase.UI
+{
+    public class MofaUIPanel : HoloKitAppUIPanel
+    {
+        public override string UIPanelName => "Mofa";
+
+        public override bool OverlayPreviousPanel => false;
+
+        [SerializeField] private RectTransform _triggerButton;
+
+        private const float RotationSpeed = 20f;
+
+        private void Update()
+        {
+            _triggerButton.Rotate(new Vector3(0f, 0f, 1f), -RotationSpeed * Time.deltaTime);
+        }
+
+        public void OnTriggerButtonPressed()
+        {
+            HoloKitAppUIEventManager.OnTriggered?.Invoke();
+        }
+    }
+}
