5a29c21464990a431632ccae2b9261537f573c15
yuchen <yuchen@holoi.com>
2023-01-07 20:57:48 +0800

    Added git lfs track for Chinese fonts

--- Packages/com.holoi.library.holokit-app/Runtime/Scripts/UI/UIPanel/HoloKitAppUIPanel_LandingPage.cs
+++ Packages/com.holoi.library.holokit-app/Runtime/Scripts/UI/UIPanel/HoloKitAppUIPanel_LandingPage.cs
@@ -43,7 +43,7 @@ namespace Holoi.Library.HoloKitApp.UI
 
             StartCoroutine(HoloKitAppUtils.WaitAndDo(PageDuration, () =>
             {
-                //LoadPermissionPage();
+                LoadPermissionPage();
             }));
         }
 
