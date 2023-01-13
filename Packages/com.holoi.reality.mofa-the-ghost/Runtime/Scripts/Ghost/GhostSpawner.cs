36dbd6b7f9c09e263b58f5df199e414b1c5afa8a
yuchen <yuchen@holoi.com>
2023-01-13 16:55:29 +0800

    Host spawns the ghost

--- Packages/com.holoi.reality.mofa-the-ghost/Runtime/Scripts/Ghost/GhostSpawner.cs
+++ Packages/com.holoi.reality.mofa-the-ghost/Runtime/Scripts/Ghost/GhostSpawner.cs
@@ -1,21 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.XR.ARFoundation;
+using Holoi.Library.ARUX;
+using Holoi.Library.HoloKitApp;
+using Holoi.Library.HoloKitApp.UI;
 
 namespace Holoi.Reality.MOFATheGhost
 {
-    public class GhostSpawner : MonoBehaviour
+    public class GhostSpawner : NetworkBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        [Header("AR")]
+        [SerializeField] private ARPlaneManager _arPlaneManager;
+
+        [SerializeField] private ARRaycastManager _arRaycastManager;
+
+        [SerializeField] private ARPlacementManager _arPlacementManager;
+
+        [Header("Ghost")]
+        [SerializeField] private Ghost _ghostPrefab;
+
+        private Ghost _ghost;
+
+        private void Awake()
         {
-        
+            HoloKitAppUIEventManager.OnStarUITriggered += OnUITriggered;
         }
 
-        // Update is called once per frame
-        void Update()
+        public override void OnDestroy()
         {
-        
+            base.OnDestroy();
+            HoloKitAppUIEventManager.OnStarUITriggered -= OnUITriggered;
+        }
+
+        public override void OnNetworkSpawn()
+        {
+            if (HoloKitApp.Instance.IsHost)
+            {
+                StartGhostPlacement();
+            }
+            else
+            {
+                _arPlacementManager.OnDisabledFunc();
+            }
+        }
+
+        /// <summary>
+        /// Call this function to 
+        /// </summary>
+        private void StartGhostPlacement()
+        {
+            _arPlaneManager.enabled = true;
+            _arRaycastManager.enabled = true;
+            _arPlacementManager.IsActive = true;
+        }
+
+        private void EndGhostPlacement()
+        {
+            _arPlaneManager.enabled = false;
+            _arRaycastManager.enabled = false;
+            _arPlacementManager.OnPlacedFunc();
+        }
+
+        private void OnUITriggered()
+        {
+            if (_ghost == null && _arPlacementManager.IsValid)
+            {
+                Transform hitPoint = _arPlacementManager.HitPoint;
+                SpawnGhost(hitPoint.position, hitPoint.rotation);
+                EndGhostPlacement();
+            }
+        }
+
+        private void SpawnGhost(Vector3 position, Quaternion rotation)
+        {
+            _ghost = Instantiate(_ghostPrefab, position, rotation);
+            _ghost.GetComponent<NetworkObject>().Spawn();
         }
     }
 }
