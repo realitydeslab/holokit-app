052a4ba4daca538c63c2620b371a092d411c7e6d
shibi <18861813833@163.com>
2023-02-09 12:18:35 +0800

    Backup

--- Packages/com.holoi.library.mofa-base/Runtime/Scripts/VFX Controller/AttackSpellVFXController.cs
+++ Packages/com.holoi.library.mofa-base/Runtime/Scripts/VFX Controller/AttackSpellVFXController.cs
@@ -1,45 +1,65 @@
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEditor;  
+//using System;
 
-namespace Holoi.Library.MOFABase
-{
-    public class AttackSpellVFXController : MonoBehaviour
-    {
-        [SerializeField] private AttackSpell _attackSpell;
+//public class CopyComponent : Editor
+//{
 
-        [SerializeField] private SpellLifetimeController _lifetimeController;
+//    static Component[] compoentArr;
 
-        [SerializeField] private Animator _animator;
+//    [MenuItem("Component Editor/Copy Component")]
+//    static void DoCopyComponent()
+//    {
+//        compoentArr = Selection.activeGameObject.GetComponents<Component>();
+//    }
 
-        private void Start()
-        {
-            _attackSpell.OnHit += OnHit;
-            _lifetimeController.OnLifetimeEnded += OnLifetimeEnded;
-        }
+//    [MenuItem("Component Editor/Paste Component")]
+//    static void DoPasteComponent()
+//    {
+//        if (compoentArr == null)
+//        {
+//            return;
+//        }
 
-        private void OnDisable()
-        {
-            // Reset
-            _animator.Rebind();
-            _animator.Update(0f);
-        }
+//        GameObject targetObject = Selection.activeGameObject;
+//        if (targetObject == null)
+//        {
+//            return;
+//        }
 
-        private void OnDestroy()
-        {
-            _attackSpell.OnHit -= OnHit;
-            _lifetimeController.OnLifetimeEnded -= OnLifetimeEnded;
-        }
-
-        public void OnHit()
-        {
-            if (_attackSpell.HitOnce)
-            {
-                _animator.SetTrigger("Hit");
-            }
-        }
-
-        public void OnLifetimeEnded()
-        {
-            _animator.SetTrigger("Die");
-        }
-    }
-}
+//        for (int i = 0; i < compoentArr.Length; i++)
+//        {
+//            Component newComponent = compoentArr[i];
+//            if (newComponent == null)
+//            {
+//                continue;
+//            }
+//            UnityEditorInternal.ComponentUtility.CopyComponent(newComponent);
+//            Component oldComponent = targetObject.GetComponent(newComponent.GetType());
+//            if (oldComponent != null)
+//            {
+//                if (UnityEditorInternal.ComponentUtility.PasteComponentValues(oldComponent))
+//                {
+//                    Debug.Log("Paste Values " + newComponent.GetType().ToString() + " Success");
+//                }
+//                else
+//                {
+//                    Debug.Log("Paste Values " + newComponent.GetType().ToString() + " Failed");
+//                }
+//            }
+//            else
+//            {
+//                if (UnityEditorInternal.ComponentUtility.PasteComponentAsNew(targetObject))
+//                {
+//                    Debug.Log("Paste New Values " + newComponent.GetType().ToString() + " Success");
+//                }
+//                else
+//                {
+//                    Debug.Log("Paste New Values " + newComponent.GetType().ToString() + " Failed");
+//                }
+//            }
+//        }
+//    }
+//}
